af85e7548942e4dc204a7f75d3c841d82a832a93
Mark Cornelissen <mark@boxture.com>
2020-04-24 16:53:20 +0200

    Got the basics working

--- lib/vario/api.rb
+++ lib/vario/api.rb
@@ -5,55 +5,95 @@ require 'grape-swagger-entity'
 
 module Vario
   class API < Grape::API
+    default_format :json
+    format :json
+
     helpers do
       def settable
         configuration[:settable]
       end
 
+      def find_settable(id)
+        record = settable.find(id)
+        error!(404, "#{settable.name} not found") unless record.present?
+        record
+      end
+
       def settable_settings
         Vario.config.settable_settings[settable.name]
+      end
+
+      def settable_setting(setting)
+        settable_settings[setting]
+      end
+
+      def settable_setting_context(setting, context)
+        vario_setting = settable_setting(setting)
+        context.symbolize_keys!
+        vario_setting[:keys].each do |key|
+          context[key] ||= nil
+        end
+        context
       end
     end
 
     desc 'Get settings'
-    get '/' do
+    get '/all_settings' do
       settable_settings
     end
 
-    desc 'Get setting for'
+    desc 'Get setting value'
     params do
       requires :id, type: String
-      requires :setting, type: String
-      optional :context, type: Hash #
+      requires :setting, type: String, documentation: { in: 'body' }
+      requires :context, type: Hash, documentation: { in: 'body' }
     end
-    get '/:id/:setting' do
+    post '/:id/get' do
+      record  = find_settable(declared_params[:id])
+      context = settable_setting_context(declared_params[:setting], declared_params[:context].to_h)
+
+      { setting: declared_params[:setting], value: record.setting(declared_params[:setting], context) }
     end
 
-    desc 'Add a setting'
+    desc 'Set setting value'
     params do
       requires :id, type: String
-      requires :setting, type: String
-      optional :context, type: Hash #
+      requires :setting, type: String, documentation: { in: 'body' }
+      requires :context, type: Hash, documentation: { in: 'body' }
+      requires :value, type: String, documentation: { in: 'body' }
     end
-    post '/:id/:setting' do
+    post '/:id/set' do
+      record        = find_settable(declared_params[:id])
+      context       = settable_setting_context(declared_params[:setting], declared_params[:context].to_h)
+      vario_setting = record.settings.find_or_initialize_by(name: declared_params[:setting])
+      level         = vario_setting.levels.find { |level| context == level.conditions_hash }
+
+      if level.nil?
+        level = Level.new(vario_setting, { conditions: context })
+        vario_setting.levels.unshift level
+      end
+
+      level.value = declared_params[:value]
+      vario_setting.save!
+
+      { setting: declared_params[:setting], value: record.setting(declared_params[:setting], context) }
     end
 
-    desc 'Remove a setting'
+    desc 'Remove setting value'
     params do
       requires :id, type: String
-      requires :setting, type: String
-      optional :context, type: Hash #
+      requires :setting, type: String, documentation: { in: 'body' }
+      requires :context, type: Hash, documentation: { in: 'body' }
     end
-    delete '/:id/:setting' do
-    end
+    delete '/:id' do
+      record  = find_settable(declared_params[:id])
+      context = settable_setting_context(declared_params[:setting], declared_params[:context].to_h)
 
-    desc 'Update a setting'
-    params do
-      requires :id, type: String
-      requires :setting, type: String
-      optional :context, type: Hash #
-    end
-    put '/:id/:setting' do
+      vario_setting = record.settings.find_or_initialize_by(name: declared_params[:setting])
+      vario_setting.levels.reject! { |level| context == level.conditions_hash }
+      vario_setting.save!
+
+      { setting: declared_params[:setting], value: record.setting(declared_params[:setting], context) }
     end
   end
 end
