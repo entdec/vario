fa77851ef318100d8cc4927eb5987a601628e460
Mark Cornelissen <mark@boxture.com>
2020-04-30 13:17:37 +0200

    Allow a prefix in the routes

--- lib/vario/api.rb
+++ lib/vario/api.rb
@@ -56,82 +56,84 @@ module Vario
 
         group_name, short_setting_name  = setting_name.split('.', 2)
         route_param :id, type: String do
-          namespace group_name do
-            desc "Get setting value" do
-              detail "Get the setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
-            end
-            params do
-              setting_data[:keys].each do |context_key|
-                optional context_key, type: String
+          namespace configuration[:prefix] do
+            namespace group_name do
+              desc "Get setting value" do
+                detail "Get the setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
               end
-            end
-            oauth2(configuration[:oauth2], "#{configuration[:oath2]}:read") if configuration[:oauth2]
-            get "/#{short_setting_name}" do
-              record  = find_settable(declared_params[:id])
-              context = settable_setting_context(setting_name, declared_params.reject { |key, value| key == 'id' }.to_h )
+              params do
+                setting_data[:keys].each do |context_key|
+                  optional context_key, type: String
+                end
+              end
+              oauth2(configuration[:oauth2], "#{configuration[:oath2]}:read") if configuration[:oauth2]
+              get "/#{short_setting_name}" do
+                record  = find_settable(declared_params[:id])
+                context = settable_setting_context(setting_name, declared_params.reject { |key, value| key == 'id' }.to_h )
 
-              { setting: setting_name, value: record.setting(setting_name, context) }
-            end
-
-            setting_type =
-              case setting_data[:type]
-              when :array
-                Array[String]
-              when :boolean
-                Boolean
-              when :integer
-                Integer
-              else
-                String
+                { setting: setting_name, value: record.setting(setting_name, context) }
               end
 
-            desc "Add or change setting value" do
-              detail "Add or change setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
-            end
-            params do
-              setting_data[:keys].each do |context_key|
-                optional context_key, type: String, documentation: { in: 'body' }
+              setting_type =
+                case setting_data[:type]
+                when :array
+                  Array[String]
+                when :boolean
+                  Boolean
+                when :integer
+                  Integer
+                else
+                  String
+                end
+
+              desc "Add or change setting value" do
+                detail "Add or change setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
               end
-              requires :value, type: setting_type, values: setting_data[:collection].present? ? setting_data[:collection].map { |item| item.last.to_s } : nil, documentation: { in: 'body' }
-            end
-            oauth2(configuration[:oauth2], "#{configuration[:oath2]}:write") if configuration[:oauth2]
-            route [:post, :put], "/#{short_setting_name}" do
-              record          = find_settable(declared_params[:id])
-              context         = settable_setting_context(setting_name, declared_params.reject { |key, value| %w[id value].include?(key) }.to_h )
-              vario_setting   = record.settings.find_or_initialize_by(name: setting_name)
-              conditions_hash = context.values.compact.blank? ? {} : context
-              level           = vario_setting.levels.find { |level| conditions_hash == level.conditions_hash }
+              params do
+                setting_data[:keys].each do |context_key|
+                  optional context_key, type: String, documentation: { in: 'body' }
+                end
+                requires :value, type: setting_type, values: setting_data[:collection].present? ? setting_data[:collection].map { |item| item.last.to_s } : nil, documentation: { in: 'body' }
+              end
+              oauth2(configuration[:oauth2], "#{configuration[:oath2]}:write") if configuration[:oauth2]
+              route [:post, :put], "/#{short_setting_name}" do
+                record          = find_settable(declared_params[:id])
+                context         = settable_setting_context(setting_name, declared_params.reject { |key, value| %w[id value].include?(key) }.to_h )
+                vario_setting   = record.settings.find_or_initialize_by(name: setting_name)
+                conditions_hash = context.values.compact.blank? ? {} : context
+                level           = vario_setting.levels.find { |level| conditions_hash == level.conditions_hash }
 
-              if level.nil?
-                level = Level.new(vario_setting, { conditions: context })
-                vario_setting.levels.unshift level
+                if level.nil?
+                  level = Level.new(vario_setting, { conditions: context })
+                  vario_setting.levels.unshift level
+                end
+
+                level.value = declared_params[:value]
+                vario_setting.save!
+
+                { setting: setting_name, value: record.setting(setting_name, context) }
               end
 
-              level.value = declared_params[:value]
-              vario_setting.save!
-
-              { setting: setting_name, value: record.setting(setting_name, context) }
-            end
-
-            desc "Remove setting value" do
-              detail "Remove setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
-            end
-            params do
-              setting_data[:keys].each do |context_key|
-                optional context_key, type: String
+              desc "Remove setting value" do
+                detail "Remove setting value for #{setting_name}. Setting description: #{setting_data[:description]}"
               end
-            end
-            oauth2(configuration[:oauth2], "#{configuration[:oath2]}:write") if configuration[:oauth2]
-            delete "/#{short_setting_name}" do
-              record           = find_settable(declared_params[:id])
-              context          = settable_setting_context(setting_name, declared_params.reject { |key, value| key == 'id' }.to_h )
-              conditions_hash  = context.values.compact.blank? ? {} : context
+              params do
+                setting_data[:keys].each do |context_key|
+                  optional context_key, type: String
+                end
+              end
+              oauth2(configuration[:oauth2], "#{configuration[:oath2]}:write") if configuration[:oauth2]
+              delete "/#{short_setting_name}" do
+                record           = find_settable(declared_params[:id])
+                context          = settable_setting_context(setting_name, declared_params.reject { |key, value| key == 'id' }.to_h )
+                conditions_hash  = context.values.compact.blank? ? {} : context
 
-              vario_setting = record.settings.find_or_initialize_by(name: setting_name)
-              vario_setting.levels.reject! { |level| conditions_hash == level.conditions_hash }
-              vario_setting.save!
+                vario_setting = record.settings.find_or_initialize_by(name: setting_name)
+                vario_setting.levels.reject! { |level| conditions_hash == level.conditions_hash }
+                vario_setting.save!
 
-              { setting: setting_name, value: record.setting(setting_name, context) }
+                { setting: setting_name, value: record.setting(setting_name, context) }
+              end
             end
           end
         end
