d9fcfacf6724f2a9c8edc11ed8e2ba5ff4ca94eb
Mark Cornelissen <mark@boxture.com>
2020-05-01 15:06:57 +0200

    Test delete

--- test/dummy/test/controllers/api_test.rb
+++ test/dummy/test/controllers/api_test.rb
@@ -8,7 +8,6 @@ class ApiTest < ActiveSupport::TestCase
   test 'get the hide_couriers setting' do
     get "/api/account_settings/#{accounts(:boxture).id}/hide_couriers"
     assert last_response.ok?
-
     assert_equal 'hide_couriers', last_json['setting']
     assert_equal true, last_json['value']
   end
@@ -16,9 +15,30 @@ class ApiTest < ActiveSupport::TestCase
   test 'set the hide_couriers setting' do
     post "/api/account_settings/#{accounts(:boxture).id}/hide_couriers", JSON.dump({ value: false }), { 'CONTENT_TYPE' => 'application/json' }
     assert last_response.created?
-
     assert_equal 'hide_couriers', last_json['setting']
     assert_equal false, last_json['value']
+
+    get "/api/account_settings/#{accounts(:boxture).id}/hide_couriers"
+    assert last_response.ok?
+    assert_equal 'hide_couriers', last_json['setting']
+    assert_equal false, last_json['value']
+  end
+
+  test 'delete removes hide_couriers setting' do
+    post "/api/account_settings/#{accounts(:boxture).id}/hide_couriers", JSON.dump({ value: false }), { 'CONTENT_TYPE' => 'application/json' }
+    assert last_response.created?
+    assert_equal 'hide_couriers', last_json['setting']
+    assert_equal false, last_json['value']
+
+    delete "/api/account_settings/#{accounts(:boxture).id}/hide_couriers", JSON.dump({ value: false }), { 'CONTENT_TYPE' => 'application/json' }
+    assert last_response.ok?
+    assert_equal 'hide_couriers', last_json['setting']
+    assert_equal true, last_json['value']
+
+    get "/api/account_settings/#{accounts(:boxture).id}/hide_couriers"
+    assert last_response.ok?
+    assert_equal 'hide_couriers', last_json['setting']
+    assert_equal true, last_json['value']
   end
 
   def app
