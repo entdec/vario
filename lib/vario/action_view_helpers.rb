ae76b51d8ec9bbb7cf0d2c0d47509c74739e8a5c
Mark Cornelissen <mark@boxture.com>
2020-11-09 14:13:01 +0100

    Enable making a single block for different settables in one go

--- lib/vario/action_view_helpers.rb
+++ lib/vario/action_view_helpers.rb
@@ -2,11 +2,29 @@
 
 module Vario
   module ActionViewHelpers
-    def render_vario_settings(settable, *setting_names, **context)
-      Vario.config.pre_create_settings(settable)
-      settings = settable.settings.where(name: setting_names)
+    def render_vario_settings(*args)
+      settings_with_contexts = []
+
+      args = [args] unless args.first.is_a?(Array)
+      args.each do |argument_set|
+        settable = argument_set.first
+
+        if argument_set.last.is_a?(Hash)
+          setting_names = argument_set[1..-2]
+          context       = argument_set.last
+        else
+          setting_names = argument_set[1..-1]
+          context       = {}
+        end
+
+        Vario.config.pre_create_settings(settable)
+        settable.settings.where(name: setting_names).each do |setting|
+          settings_with_contexts << { setting: setting, context: context }
+        end
+      end
+
       render partial: 'vario/settings/settings', locals: {
-        settings: settings, context: context
+        settings_with_contexts: settings_with_contexts
       }
     end
   end
